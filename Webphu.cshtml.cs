f054a9a8bbff3c31fac47a52b279f6a28b4add0c
datnb999-dot <datnb999@gmail.com>
2026-02-05 09:09:59 +0700

    Add files via upload

---
+++ Webphu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace quatang1.Pages
+{
+    public class WebphuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
